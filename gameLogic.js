32727c8d282234cb0f0500280d51b909bf794446
Zac McClain <zamcclai@mines.edu>
2014-09-07 17:23:35 -0600

    I renamed the main HTML page to Team01F14.html (specifeyed in the requirements section of team1 assinment as we are team number '01'). I also added a skeleton .js script and linked it to our HTML I did not add any functionality. Finaly I made a template for our CSS and HTML so we have a good idea of what we are looking at.

---
+++ gameLogic.js
@@ -0,0 +1,52 @@
+// Globel Variables:
+var int score = 0; / 
+
+// Start of Script:
+
+window.onload = function() {
+	
+	//Create canvase
+
+}
+
+function start() {
+
+	// Click on the start button that will be in the middle canvase.
+	// this method should also be use to restart a new game after finishing an old one.
+}
+
+function flyingFruit() {
+
+	// May need to break this up into more methods
+	// If will display the fruit flying from one side of the screen to the other
+	// using projectile motion.
+}
+
+function veggieDestruction() {
+
+	// will display the flying veggis when being chopped and splatting on the screen.
+	// this will likely also update the score or will call that method.
+}
+
+function bomHit() {
+
+	
+}
+
+function chopTrail() {
+
+	// this will display the trail that fallows the mouse when it is "chopping"
+	// accross the screen. we may need to do this in differnt method or place.
+}
+
+function displayScore(){
+
+	//Function for displaying the score
+}
+
+function updateScore(int score){
+
+	//Fuction for updateing score
+}
+
+

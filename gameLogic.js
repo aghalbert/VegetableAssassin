c605f965b34bca7b64b152734355208cbd12e399
Maya Gort <mgort@mymail.mines.edu>
2014-09-16 12:24:57 -0600

    Implemented click to start

--- gameLogic.js
+++ gameLogic.js
@@ -12,7 +12,8 @@ function setCanvasVars() {
 	context = canvas.getContext("2d");
 	midX = canvas.width/2;
 	midY = canvas.height/2;
-	}
+	canvas.addEventListener("mousedown", start);
+}
 	
 function loadCanvas() {
 	context.font = "30px Arial";
@@ -21,9 +22,10 @@ function loadCanvas() {
 }
 
 function start() {
-
 	// Click on the start button that will be in the middle canvas.
 	// this method should also be use to restart a new game after finishing an old one.
+	context.clearRect(0, 0, canvas.width, canvas.height);
+	window.setTimeout(gameOver, 3000); //Change this back to 30000  
 }
 
 function flyingVeggies() {
@@ -35,7 +37,7 @@ function flyingVeggies() {
 
 function veggieDestruction() {
 
-	// will display the flying veggis when being chopped and splatting on the screen.
+	// will display the flying veggies when being chopped and splatting on the screen.
 	// this will likely also update the score or will call that method.
 }
 
@@ -46,18 +48,27 @@ function bomHit() {
 
 function chopTrail() {
 
-	// this will display the trail that fallows the mouse when it is "chopping"
+	// this will display the trail that follows the mouse when it is "chopping"
 	// accross the screen. we may need to do this in differnt method or place.
 }
 
-function displayScore(){
+function displayScore() {
 
 	//Function for displaying the score
 }
 
-function updateScore(score){
+function updateScore(score) {
 
-	//Fuction for updateing score
+	//Fuction for updating score
+}
+
+function gameOver() {
+	context.clearRect(0, 0, canvas.width, canvas.height);
+	context.font = "50px Arial";
+	context.fillText("GAME OVER", midX-50, midY);
+	context.font = "30px Arial";
+	context.fillText(score, midX, midY + 60);
+	context.fillText("Click to play again!", midX  - 30, midY + 120);
 }
 
 
